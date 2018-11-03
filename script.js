678285b26b6fe9fdd38634d3bb014cbafc5fff06
Brandy McArthur <bdm.mcarthur@gmail.com>
2018-11-03 15:31:13 -0400

    Added random color picking to grid

---
+++ script.js
@@ -0,0 +1,27 @@
+// var re = document.getElementById("rgb")
+// var1 = Math.random() * 255
+// var2 = Math.random() * 255
+// var3 = Math.random() * 255
+// re.style.color="rgb" + "(" + var1.toString() + "," + var2.toString() + "," + var3.toString() + ")"
+//
+// var bg = re.style.color
+// console.log(bg)
+
+var re = document.getElementsByClassName('square')
+
+function randomColor(){
+    for (let i = 0; i < re.length; i++) {
+        var1 =
+        var2 = Math.random() * 255
+        var3 = Math.random() * 255
+      re[0].style.backgroundColor = "rgb" + "(" + parseInt(Math.random() * 255) + "," + parseInt(Math.random() * 255) + "," + parseInt(Math.random() * 255) + ")"
+      re[1].style.backgroundColor = "rgb" + "(" + parseInt(Math.random() * 255) + "," + parseInt(Math.random() * 255) + "," + parseInt(Math.random() * 255) + ")"
+      re[2].style.backgroundColor = "rgb" + "(" + parseInt(Math.random() * 255) + "," + parseInt(Math.random() * 255) + "," + parseInt(Math.random() * 255) + ")"
+      re[3].style.backgroundColor = "rgb" + "(" + parseInt(Math.random() * 255) + "," + parseInt(Math.random() * 255) + "," + parseInt(Math.random() * 255) + ")"
+      re[4].style.backgroundColor = "rgb" + "(" + parseInt(Math.random() * 255) + "," + parseInt(Math.random() * 255) + "," + parseInt(Math.random() * 255) + ")"
+      re[5].style.backgroundColor = "rgb" + "(" + parseInt(Math.random() * 255) + "," + parseInt(Math.random() * 255) + "," + parseInt(Math.random() * 255) + ")"
+
+}
+}
+randomColor()
+console.log(re[0].style.backgroundColor)
